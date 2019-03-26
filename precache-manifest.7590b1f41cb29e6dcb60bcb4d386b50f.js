43521543f5dd7bde0dba736d278ae33a8362531b
matthieu <matthieu.jasselette@gmail.com>
2019-03-26 16:41:06 +0100

    Updates

---
+++ precache-manifest.7590b1f41cb29e6dcb60bcb4d386b50f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-character-manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6d242375651bc27cc5a1",
+    "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
+  },
+  {
+    "revision": "78ec681e3453574e7402",
+    "url": "/react-character-manager/static/js/main.1b5487a9.chunk.js"
+  },
+  {
+    "revision": "9784aec82e4e4985596d",
+    "url": "/react-character-manager/static/js/2.7328fc16.chunk.js"
+  },
+  {
+    "revision": "78ec681e3453574e7402",
+    "url": "/react-character-manager/static/css/main.f0c9b035.chunk.css"
+  },
+  {
+    "revision": "9784aec82e4e4985596d",
+    "url": "/react-character-manager/static/css/2.2c6f0210.chunk.css"
+  },
+  {
+    "revision": "9cd9a3e3930de1b9934ece6826d2b914",
+    "url": "/react-character-manager/index.html"
+  }
+];
